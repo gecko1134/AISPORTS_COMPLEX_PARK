eb4f94c94ee54a2b7610b015ba0b6280c3d30e17
gecko1134 <85382061+gecko1134@users.noreply.github.com>
2025-06-02 23:12:31 -0500

    Add files via upload

---
+++ git_push_setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git init
+git remote add origin https://github.com/gecko1134/AISPORTS_COMPLEX_PARK.git
+git add .
+git commit -m "Initial full release of AISPORTS COMPLEX PARK SuperSuite"
+git branch -M main
+git push -u origin main
